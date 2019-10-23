60b9fdebd92d35cb8848558ebb976d0e12659ca9
Mielikki <5599742+rnielikki@users.noreply.github.com>
2019-10-23 10:35:06 +0300

    organized some tests

--- src/joulukalenteriTests/CalendarTest.cs
+++ src/joulukalenteriTests/CalendarTest.cs
@@ -1,8 +1,5 @@
 using System;
 using Xunit;
-using System.Linq;
-using System.Collections.Generic;
-using joulukalenteri.Client.Pages;
 using Pose;
 using joulukalenteri.Client.SharedCode;
 
@@ -10,41 +7,6 @@ namespace joulukalenteriTest
 {
     public class CalendarTest
     {
-        joulukalenteri.Client.Pages.Index indexpage = new joulukalenteri.Client.Pages.Index();
-        [Fact]
-        public void ShuffleTest()
-        {
-            //System.Index exists : must be written explicitly.
-            Assert.Equal(indexpage.ShuffleDays(), indexpage.ShuffleDays());
-
-            int[] shuffled = indexpage.ShuffleDays();
-
-            //without duplication
-            Assert.Equal(shuffled.Length, shuffled.Distinct().Count());
-
-            //1-25
-            Assert.Equal(1, shuffled.Min());
-            Assert.Equal(25, shuffled.Max());
-
-            //make sure that not shuffled. very rarely not succeeded.
-            Assert.NotEqual(shuffled, Enumerable.Range(1, 25).ToArray());
-
-            /*
-            Test TODO:
-            1. Random 1-25 mixing.
-                -> use YEAR as random seed. Hint: new Random(YEAR);
-            2. Check if activates according to date.
-                -> Vaihtoehto 1: "Shim" with *isolation framework*. ( Typemock, Microsoft moles, Pose, ... )
-                -> Vaihtoehto 2: Using "Moq" but do something more ( https://stackoverflow.com/questions/2425721/unit-testing-datetime-now )
-            3. Read Custom JSON file ([{day:..., title:..., content:..., image:...}, ...]) and asserts if everything Equals.
-                -> use "Theory data (inline data is ok.)"
-                -> DateTime Change trick is same as 2.
-            4. Show only some months. (not open for summer!)
-                -> Hint is same as 2.
-
-            Archive not supported :)
-             */
-        }
         [Theory]
         [MemberData(nameof(OpenByDateData))]
         //[InlineData(new DateTime(2020, 12, 21), 23, false)]
---
+++ src/joulukalenteriTests/ShuffleTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Pose;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace joulukalenteriTests
+{
+    public class ShuffleTest
+    {
+        joulukalenteri.Client.Pages.Index indexpage = new joulukalenteri.Client.Pages.Index();
+        [Fact]
+        public void ValidShuffleTest()
+        {
+            int[] shuffled = indexpage.ShuffleDays();
+            //without duplication
+            Assert.Equal(shuffled.Length, shuffled.Distinct().Count());
+
+            //1-25
+            Assert.Equal(1, shuffled.Min());
+            Assert.Equal(25, shuffled.Max());
+
+        }
+        [Fact]
+        public void ShuffleConsistenceTest() {
+            int[] shuffled = indexpage.ShuffleDays();
+            //System.Index exists : must be written explicitly.
+            Assert.Equal(indexpage.ShuffleDays(), indexpage.ShuffleDays());
+
+            //make sure that not shuffled. very rarely not succeeded.
+            Assert.NotEqual(shuffled, Enumerable.Range(1, 25).ToArray());
+
+            //Different years, different days.
+            int[] shuffled2 = new int[] { };
+            Shim shim = Shim.Replace(() => DateTime.Today).With(()=>new DateTime(DateTime.Today.Year-1,1,1));
+            PoseContext.Isolate(() => {
+                shuffled2 = indexpage.ShuffleDays();
+            }, shim);
+            Assert.NotEqual(shuffled, shuffled2);
+        }
+    }
+}
