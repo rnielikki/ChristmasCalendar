ba5390258372f0a26588624ab234cda4798ee445
rnielikki <lumi.kwon@student.lamk.fi>
2020-11-21 12:16:15 +0200

    Migrated to .NET 5

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -12,7 +12,8 @@ namespace AdventCalendar
         private static async System.Threading.Tasks.Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.Services.AddScoped(sp => new System.Net.Http.HttpClient { BaseAddress = new System.Uri(builder.HostEnvironment.BaseAddress) });
+            builder.RootComponents.Add<App>("#app");
             ConfigureServices(builder.Services);
             await builder.Build().RunAsync().ConfigureAwait(true); //Blazor has SynchronizationContext
         }
@@ -26,7 +27,7 @@ namespace AdventCalendar
             services.AddSingleton<DaysShuffler>();
 
             services.AddTransient<Validator>();
-            services.AddTransient<IDataReceiver, DataReceiver>();
+            services.AddTransient<IDataReceiver, DataReceiver>(sp => new DataReceiver(sp.GetService<IAppSettings>(), sp.GetService<Microsoft.AspNetCore.Components.NavigationManager>()));
 
             services.AddTransient<IDateTime, DefaultDateTime>();
         }
